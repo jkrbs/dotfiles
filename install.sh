0b6b2d10afb3d13c2e8edea5fc2b63b7d7be8fe8
Jakob Krebs <jakob@krbs.me>
2021-02-05 15:39:36 +0100

    update mbsync and notmuch with fsr mail account

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ ln -s $(pwd)/.gitconfig $HOME/.gitconfig
 ln -s $(pwd)/.vimrc $HOME/.vimrc
 ln -s $(pwd)/.vim $HOME/.vim
 ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/.mbsyncrc $HOME/.mbsyncrc
+ln -s $(pwd)/.notmuch-config $HOME/.notmuch-config
 
