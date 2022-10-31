555fdcfbe506797e2258c34d969a5cb15bd17895
Jakob Krebs <jakob@krbs.me>
2022-10-31 14:09:54 +0100

    add kitty config

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ ln -s $(pwd)/.vim $HOME/.vim
 ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/.mbsyncrc $HOME/.mbsyncrc
 ln -s $(pwd)/.notmuch-config $HOME/.notmuch-config
-
+ln -s $(pwd)/.doom.d $HOME/.doom.d
+ln -s $(pwd)/kitty $HOME/.config/kitty
